51af21c9c631ec1de0c0e993c3d490f099c972e7
mateiahacks <mategiorbelidze1@gmail.com>
2023-07-14 13:04:21 +0400

    setted up for production

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,13 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/comments', require('./routes/commentRoutes'));
 app.use('/api/search', require('./routes/searchRoutes'));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
